cf22c3519024889a92265e5d5d4ee3344bf060a6
OlegBirykov <p_petro75@mail.ru>
2021-04-30 10:36:27 +0300

    Инициализация проекта

---
+++ Readme.md
@@ -0,0 +1,3 @@
+### Ссылка на Heroku
+
+https://ahj-8-2.herokuapp.com
---
+++ server.js
@@ -0,0 +1,66 @@
+const http = require('http');
+const Koa = require('koa');
+const cors = require('koa2-cors');
+// const uuid = require('uuid');
+const WS = require('ws');
+
+const app = new Koa();
+
+app.use(cors());
+
+const users = new Map();
+
+const port = process.env.PORT || 7070;
+const server = http.createServer(app.callback());
+const wsServer = new WS.Server({ server });
+
+wsServer.on('connection', (ws) => {
+//  const errCallback = (err) => {
+//    if (err) {
+  // TODO: handle error
+//    }
+//  };
+  const sendUserList = () => {
+    const json = JSON.stringify({
+      event: 'users',
+      users: Array.from(users.values()),
+    });
+
+    Array.from(wsServer.clients)
+      .filter((o) => o.readyState === WS.OPEN)
+      .forEach((o) => o.send(json));
+  };
+
+  ws.on('message', (msg) => {
+    const req = JSON.parse(msg);
+    const res = {};
+
+    switch (req.event) {
+      case 'connect':
+        res.event = Array.from(users.values()).includes(req.name) ? 'noconnect' : 'connect';
+        ws.send(JSON.stringify(res));
+        if (res.event === 'noconnect') {
+          return;
+        }
+
+        users.set(ws, req.name);
+        sendUserList();
+        break;
+
+      case 'message':
+        break;
+      default:
+    }
+  });
+
+  ws.on('error', () => {
+    // console.log('ошибочка вышла');
+  });
+
+  ws.on('close', () => {
+    users.delete(ws);
+    sendUserList();
+  });
+});
+
+server.listen(port);
